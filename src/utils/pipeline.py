ed89ce02147058dfe7dd7105600624d9d8900e1b
jmisilo <jmisilo@interia.pl>
2022-06-26 13:28:07 +0200

    Create text pipeline and DataLoader

---
+++ src/utils/pipeline.py
@@ -0,0 +1,34 @@
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("siebert/sentiment-roberta-large-english")
+
+def text_pipeline(texts):
+    """
+        texts: list of strings
+
+        returns tokenized_text, attention_mask
+    """
+    tokenized_text = tokenizer(texts, padding='longest', return_tensors='pt')
+
+    return tokenized_text['input_ids'], tokenized_text['attention_mask']
+
+def collate_fn(batch):
+    texts, targets = [], []
+
+    for text, target in batch:
+        texts.append(text)
+        targets.append(target)
+
+    inputs, masks = text_pipeline(texts)
+
+    return inputs, masks, targets
+
+def get_loader(dataset, batch_size, num_workers=0, pin_memory=False):
+    return DataLoader(
+        dataset, 
+        batch_size=batch_size, 
+        collate_fn=collate_fn, 
+        num_workers=num_workers, 
+        pin_memory=pin_memory
+    )

09efb1f4e920dc75a258f7d3729e1fdc58026144
jmisilo <jmisilo@interia.pl>
2022-07-05 21:28:09 +0200

    Prepare simple /predict endpoint, that allows user to make prediction

--- src/app.py
+++ src/app.py
@@ -1,7 +1,30 @@
+import os
+import torch
 import uvicorn
+from dotenv import dotenv_values
 from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import AutoModelForSequenceClassification
+
+try:
+    from src.utils.pipeline import text_pipeline
+except:
+    from utils.pipeline import text_pipeline
 
 app = FastAPI()
+
+device = device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = AutoModelForSequenceClassification.from_pretrained('siebert/sentiment-roberta-large-english').to(device)
+
+try:
+    state_dict = torch.load(os.path.join('..', 'models', 'model.pt'), map_location=device)
+except:
+    state_dict = torch.load(os.path.join('models', 'model.pt'), map_location=device)
+
+model.load_state_dict(state_dict)
+model.eval()
+
+secret_config = dotenv_values()
 
 @app.get('/')
 def root():
@@ -10,7 +33,23 @@ def root():
         'message': 'Hello! Welcome in Offensive Language Classificator API.'
     }
 
+class Post(BaseModel):
+    text: str
+
+@app.get('/predict')
+def predict(post: Post) -> str:
+
+    tokenized_text, attention_mask = text_pipeline([post.text])
+    prediction = model(input_ids=tokenized_text, attention_mask=attention_mask)
+
+    return {
+        'text': post.text, 
+        'prediction': 'Offensive' if torch.argmax(prediction.logits).item() else 'Not Offensive'
+    }
+
 if __name__ == '__main__':
     # from root
     # uvicorn src.app:app --reload --port 5000
-    uvicorn.run('app:app', host='127.0.0.1', port=5000, log_level='info')
+    port = int(secret_config['PORT']) if secret_config['PORT'] else 5000
+
+    uvicorn.run('app:app', host='127.0.0.1', port=port, reload=True)
